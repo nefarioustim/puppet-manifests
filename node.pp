a5b8b2a8335b45bf4bf2a571bdc897972bb62f98
Tim Huegdon <tim@nefariousdesigns.co.uk>
2012-04-09 11:03:28 +0100

    Updating node manifest to use aptrepo.pp.

--- node.pp
+++ node.pp
@@ -1,8 +1,20 @@
+import "aptrepo.pp"
+
+class chrisleappa {
+    pparepo { "chris-lea/node.js":
+        apt_key => "C7917B12",
+        ensure => present
+    }
+}
+
 class nodejs {
+    require chrisleappa
+
     package { [
             "nodejs",
             "npm"
         ]:
-        ensure => latest
+        ensure => latest,
+        require => Class['chrisleappa']
     }
 }
