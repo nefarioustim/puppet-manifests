a2a1b547d31d615173284cd6230da513dc28d607
Tim Huegdon <tim@nefariousdesigns.co.uk>
2013-10-13 16:03:44 +0100

    Add Python module and remove python.pp.

---
+++ modules/python/manifests/init.pp
@@ -0,0 +1,68 @@
+class python {
+    package { [
+            'python',
+            'python-dev',
+            'python-setuptools'
+        ]:
+        ensure => latest,
+    }
+    exec { 'install-pip':
+        command => 'easy_install pip',
+        require => Package['python-setuptools'],
+    }
+}
+
+class python::virtualenv {
+    package { 'virtualenv':
+        ensure => latest,
+        provider => pip,
+        require => Exec['install-pip'],
+    }
+}
+
+class python::fabric {
+    package { 'fabric':
+        ensure => latest,
+        provider => pip,
+        require => Exec['install-pip'],
+    }
+}
+
+class python::gunicorn {
+    file { "/var/log/gunicorn":
+        ensure => "directory",
+        owner  => vagrant,
+        group  => vagrant,
+        mode   => 655,
+    }
+}
+
+class python::supervisord {
+    package { 'supervisor':
+        ensure => latest,
+        provider => pip,
+        require => Exec['install-pip'],
+    }
+    file { "/etc/supervisor.d":
+        ensure => directory,
+        owner => root,
+        group => root,
+        mode => 655,
+    }
+    file { "/etc/supervisord.conf":
+        owner  => root,
+        group  => root,
+        mode   => 644,
+        source => "puppet:///modules/python/supervisord.conf",
+    }
+    file { "/etc/init.d/supervisord":
+        owner  => root,
+        group  => root,
+        mode   => 655,
+        source => "puppet:///modules/python/supervisord.initd",
+    }
+    service { "supervisord":
+        ensure => stopped,
+        hasrestart => true,
+    }
+}
