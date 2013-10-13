d2391d27de259725cfade595dea5c39828fa3ef8
Tim Huegdon <tim@nefariousdesigns.co.uk>
2013-10-13 10:46:05 +0100

    Add nodejs module and remove node.pp.

--- modules/nodejs/manifests/init.pp
+++ modules/nodejs/manifests/init.pp
@@ -1,7 +1,7 @@
-import "aptrepo.pp"
-
 class chrisleappa {
-    pparepo { "chris-lea/node.js":
+    include apt
+
+    apt::pparepo { "chris-lea/node.js":
         apt_key => "C7917B12",
         ensure => present
     }
@@ -10,10 +10,7 @@ class chrisleappa {
 class nodejs {
     require chrisleappa
 
-    package { [
-            "nodejs",
-            "npm"
-        ]:
+    package { "nodejs":
         ensure => latest,
         require => Class['chrisleappa']
     }
