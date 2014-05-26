5941e062c6462bc11540b2630531ccab8787094e
Tim Huegdon <tim@nefariousdesigns.co.uk>
2014-05-26 08:32:56 +0100

    Update user module.

--- modules/user/manifests/init.pp
+++ modules/user/manifests/init.pp
@@ -10,9 +10,9 @@ class user($username, $groupname) {
         ],
         require => Group[$groupname]
     }
-    user { "www-user":
+    user { "www-data":
         ensure => present,
-        groups => [ "sudo" ]
+        groups => [$groupname],
     }
     file { "/home/${username}/.bashrc":
         ensure => present,
@@ -53,5 +53,37 @@ class user($username, $groupname) {
         mode   => 644,
         content => template("user/bash_aliases.erb"),
         require => User[$username]
+    }
+    file { "aws-config-path":
+        path   => "/home/${username}/.aws",
+        ensure => directory,
+        owner  => $username,
+        group  => $groupname,
+        mode   => 644,
+        require => User[$username]
+    }
+    file { "aws-config-path-root":
+        path   => "/root/.aws",
+        ensure => directory,
+        mode   => 644,
+    }
+    file { "aws-config":
+        path   => "/home/${username}/.aws/config",
+        ensure => present,
+        owner  => $username,
+        group  => $groupname,
+        mode   => 644,
+        content => template("user/awsconfig.erb"),
+        require => [
+            User[$username],
+            File["aws-config-path"]
+        ]
+    }
+    file { "aws-config-root":
+        path   => "/root/.aws/config",
+        ensure => present,
+        mode   => 644,
+        content => template("user/awsconfig.erb"),
+        require => File["aws-config-path"]
     }
 }
