9044cb006d178ac6aa38f24d8e246ea391433b39
Tim Huegdon <tim@nefariousdesigns.co.uk>
2014-08-01 11:43:21 +0100

    Update nginx repo to assume inclusion of Forge apt module and use that to get the right package.

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,32 +1,60 @@
 class nginx {
+    package { "nginx":
+        ensure      => latest,
+        require     => Anchor['nginx::apt_repo'],
+    }
+
+    anchor { 'nginx::apt_repo' : }
+
+    apt::source { 'nginx':
+        location    => "http://nginx.org/packages/${lsbdistcodename}",
+        repos       => 'nginx',
+        key         => '7BD9BF62',
+        key_source  => 'http://nginx.org/keys/nginx_signing.key',
+        notify      => Exec['apt_get_update_for_nginx'],
+    }
+
+    exec { 'apt_get_update_for_nginx':
+        command     => '/usr/bin/apt-get update',
+        timeout     => 240,
+        returns     => [ 0, 100 ],
+        refreshonly => true,
+        before      => Anchor['nginx::apt_repo'],
+    }
+
     file { "/etc/nginx/nginx.conf":
-        owner  => root,
-        group  => root,
-        mode   => 644,
-        content => template("nginx/nginx.conf.erb"),
-        notify => Service["nginx"]
+        owner       => root,
+        group       => root,
+        mode        => 644,
+        content     => template("nginx/nginx.conf.erb"),
+        notify      => Service["nginx"],
+        require     => Package["nginx"]
     }
 
     file { "/etc/nginx/conf.d/upstream.conf":
-        owner  => root,
-        group  => root,
-        mode   => 644,
-        content => template("nginx/upstream.conf.erb"),
-        notify => Service["nginx"]
+        owner       => root,
+        group       => root,
+        mode        => 644,
+        content     => template("nginx/upstream.conf.erb"),
+        notify      => Service["nginx"],
+        require     => Package["nginx"]
     }
 
     file { "/etc/nginx/conf.d/default.conf":
-        ensure => absent,
-        notify => Service["nginx"]
+        ensure      => absent,
+        notify      => Service["nginx"],
+        require     => Package["nginx"]
     }
 
     file { "/etc/nginx/conf.d/example_ssl.conf":
-        ensure => absent,
-        notify => Service["nginx"]
+        ensure      => absent,
+        notify      => Service["nginx"],
+        require     => Package["nginx"]
     }
 
     service { "nginx":
-        ensure => running,
-        hasrestart => true
+        ensure      => running,
+        hasrestart  => true,
+        require     => Package["nginx"]
     }
 }
