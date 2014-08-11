9e7711aca3fd7a1cd1bcd53c67e214c89bcc1c6b
Tim Huegdon <tim@nefariousdesigns.co.uk>
2014-08-11 19:33:51 +0100

    Update execs to inherit user in name.

--- modules/mysql/manifests/user.pp
+++ modules/mysql/manifests/user.pp
@@ -1,12 +1,12 @@
 define mysql::user($user = $title, $pass) {
-    exec { "create-user-all":
+    exec { "create-user-${user}-all":
         unless => "mysql -uroot -p${mysql::root_password} mysql -e \"SELECT user, host FROM user WHERE user='${user}' AND host='%';\"",
         path => "/bin:/usr/bin",
         command => "mysql -uroot -p${mysql::root_password} -e \"CREATE USER ${user}@'%' IDENTIFIED BY '${pass}'; GRANT ALL ON *.* TO ${user}@'%' WITH GRANT OPTION;\"",
         require => Class["mysql"],
     }
 
-    exec { "create-user-localhost":
+    exec { "create-user-${user}-localhost":
         unless => "mysql -uroot -p${mysql::root_password} mysql -e \"SELECT user, host FROM user WHERE user='${user}' AND host='localhost';\"",
         path => "/bin:/usr/bin",
         command => "mysql -uroot -p${mysql::root_password} -e \"CREATE USER ${user}@'localhost' IDENTIFIED BY '${pass}'; GRANT ALL ON *.* TO ${user}@'localhost' WITH GRANT OPTION;\"",
