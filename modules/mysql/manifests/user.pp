98920d41e62a5bbdfd99bae324e10887260c03d8
Tim Huegdon <tim@nefariousdesigns.co.uk>
2014-08-11 19:29:50 +0100

    Switch it to a Defined Type, you numpty.

--- modules/mysql/manifests/user.pp
+++ modules/mysql/manifests/user.pp
@@ -1,4 +1,4 @@
-class mysql::user($user, $pass) {
+define mysql::user($user, $pass) {
     exec { "create-user-all":
         unless => "mysql -uroot -p${mysql::root_password} mysql -e \"SELECT user, host FROM user WHERE user='${user}' AND host='%';\"",
         path => "/bin:/usr/bin",
