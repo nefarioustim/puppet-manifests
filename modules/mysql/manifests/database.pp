cee3fd7e3dd864d3cc9ea97c21ec1295fa054998
Tim Huegdon <tim@nefariousdesigns.co.uk>
2014-08-12 19:50:18 +0100

    Fix the name issue. Duh.

--- modules/mysql/manifests/database.pp
+++ modules/mysql/manifests/database.pp
@@ -1,5 +1,5 @@
 define mysql::database($db = $title) {
-    exec { "create-user-${user}-all":
+    exec { "create-db-${db}":
         unless => "mysql -uroot -p${mysql::root_password} -e \"SHOW DATABASES;\" | grep ${db}",
         path => "/bin:/usr/bin",
         command => "mysql -uroot -p${mysql::root_password} -e \"CREATE DATABASE ${db};\"",
