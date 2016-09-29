5ac3a4e3ff45cf114d0afb236ebadf01d1ba7f8f
Tim Huegdon <tim@nefariousdesigns.co.uk>
2016-09-29 11:54:34 +0100

    Complete overhaul and update of the exim module to allow for SendGrid.

--- modules/exim/manifests/init.pp
+++ modules/exim/manifests/init.pp
@@ -1,14 +1,36 @@
 class exim {
-    package { "exim4":
-        ensure => latest
+    package { 'postfix':
+        ensure => absent
     }
+
+    package { "exim4":
+        ensure => present
+    }
+
+    service { "exim4":
+        ensure  => running,
+        require => Package["exim4"]
+    }
+
 
     file { "/etc/exim4/update-exim4.conf.conf":
         content => template("exim/update-exim4.conf.conf.erb"),
         require => Package["exim4"]
     }
 
-    exec { "update-exim4.conf":
-        require => File["/etc/exim4/update-exim4.conf.conf"]
+    file { "/etc/exim4/exim4.conf.localmacros":
+        content => template("exim4.conf.localmacros.erb"),
+        require => Package["exim4"]
+    }
+
+    file { "/etc/exim4/passwd.client":
+        content => template("exim/passwd.client.erb"),
+        require => Package["exim4"]
+    }
+
+    exec { "/usr/sbin/update-exim4.conf":
+        subscribe   => File["/etc/exim4/update-exim4.conf.conf"],
+        refreshonly => true,
+        require     => Package["exim4"]
     }
 }
