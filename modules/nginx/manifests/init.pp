0224700b7f4e2d4929b3935d30a01978b318b68c
Tim Huegdon <tim@nefariousdesigns.co.uk>
2016-10-09 18:30:33 +0100

    Update nginx manifests and delete a bunch of outdated stuff.

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,28 +1,18 @@
 class nginx($upstream) {
+    include apt
     include instassl
 
-    package { "nginx":
-        ensure      => latest,
-        require     => Anchor['nginx::apt_repo'],
-    }
-
-    anchor { 'nginx::apt_repo' : }
-
-    apt::source { 'nginx':
+    apt::source { "nginx":
         location    => 'http://nginx.org/packages/mainline/ubuntu/',
         release     => $lsbdistcodename,
         repos       => 'nginx',
         key         => '7BD9BF62',
-        key_source  => 'http://nginx.org/keys/nginx_signing.key',
-        notify      => Exec['apt_get_update_for_nginx'],
+        key_source  => 'http://nginx.org/keys/nginx_signing.key'
     }
 
-    exec { 'apt_get_update_for_nginx':
-        command     => '/usr/bin/apt-get update',
-        timeout     => 240,
-        returns     => [ 0, 100 ],
-        refreshonly => true,
-        before      => Anchor['nginx::apt_repo'],
+    package { "nginx":
+        ensure      => latest,
+        require     => Apt::Source['nginx'],
     }
 
     file { "/etc/nginx/nginx.conf":
@@ -35,15 +25,6 @@ class nginx($upstream) {
             Class["instassl"],
             Package["nginx"]
         ]
-    }
-
-    file { "/etc/nginx/conf.d/upstream.conf":
-        owner       => root,
-        group       => root,
-        mode        => "0644",
-        content     => template("nginx/upstream.conf.erb"),
-        notify      => Service["nginx"],
-        require     => Package["nginx"]
     }
 
     file { "/etc/nginx/conf.d/default.conf":
