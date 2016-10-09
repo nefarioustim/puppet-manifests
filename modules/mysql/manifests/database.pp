af425c27ba8bfa7b783724dac3f5d207b85cb7ea
Tim Huegdon <tim@nefariousdesigns.co.uk>
2016-10-09 18:48:36 +0100

    Delete lots of stuff from Tim's original repo that we don't need.

--- modules/mysql/manifests/database.pp
+++
@@ -1,8 +0,0 @@
-define mysql::database($db = $title) {
-    exec { "create-db-${db}":
-        unless => "mysql -uroot -p${mysql::root_password} -e \"SHOW DATABASES;\" | grep ${db}",
-        path => "/bin:/usr/bin",
-        command => "mysql -uroot -p${mysql::root_password} -e \"CREATE DATABASE ${db};\"",
-        require => Class["mysql"],
-    }
-}
--- modules/supervisorprog/manifests/init.pp
+++
@@ -1,8 +0,0 @@
-define supervisorprog($user, $command, $directory, $numprocs = 1, $autostart = true, $enviro = '') {
-    include python::supervisor
-
-    file { "/etc/supervisor.d/${title}.conf":
-        content => template("supervisorprog/program.conf.erb"),
-        notify => Service["supervisord"]
-    }
-}
