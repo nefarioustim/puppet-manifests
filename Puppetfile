1261105f3b188249536b00c98867e7498e76a2ca
Tim Huegdon <tim@nefariousdesigns.co.uk>
2016-10-09 17:50:06 +0100

    Add SSL module.

--- Puppetfile
+++ Puppetfile
@@ -3,6 +3,7 @@ forge "https://forgeapi.puppetlabs.com"
 mod "puppetlabs-apt"
 mod "puppetlabs-ntp"
 mod "saz-timezone"
+mod "camptocamp-openssl"
 
 mod "attachmentgenie/ufw",
     :git => "git://github.com/attachmentgenie/attachmentgenie-ufw.git"
---
+++ modules/ssl/manifests/init.pp
@@ -0,0 +1,14 @@
+class ssl {
+    class { '::openssl':
+        package_ensure         => latest,
+        ca_certificates_ensure => latest,
+    }
+
+    openssl::dhparam { '/etc/ssl/certs/dhparam.pem':
+        ensure => 'present',
+        size   => 4096,
+        owner  => 'www-data',
+        group  => 'www-data',
+        mode   => '0640',
+    }
+}
