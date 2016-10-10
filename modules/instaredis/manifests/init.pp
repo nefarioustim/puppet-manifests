6e151ef3dd3e6977f1c6c66ab4d2be2521c7af53
Tim Huegdon <tim@nefariousdesigns.co.uk>
2016-10-10 11:13:54 +0100

    Temporary disable of redis conf and change of PPA.

--- modules/instaredis/manifests/init.pp
+++ modules/instaredis/manifests/init.pp
@@ -1,17 +1,17 @@
 class instaredis {
-    apt::ppa { "ppa:rwky/redis": }
+    apt::ppa { "ppa:chris-lea/redis-server": }
 
     package { "redis-server":
         ensure => latest,
-        require => Apt::Ppa["ppa:rwky/redis"],
+        require => Apt::Ppa["ppa:chris-lea/redis-server"],
     }
 
-    file { "/etc/redis/redis.conf":
-        ensure => file,
-        content => template("redis/redis.conf.erb"),
-        require => Package["redis-server"],
-        notify => Service["redis-server"]
-    }
+    # file { "/etc/redis/redis.conf":
+    #     ensure => file,
+    #     content => template("redis/redis.conf.erb"),
+    #     require => Package["redis-server"],
+    #     notify => Service["redis-server"]
+    # }
 
     service { "redis-server":
         ensure => running,
