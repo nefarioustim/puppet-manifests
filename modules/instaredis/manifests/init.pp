7382664d8a4e08f58985d556c1b19982af5d44c0
Tim Huegdon <tim@nefariousdesigns.co.uk>
2016-10-11 10:34:07 +0100

    Go back to the Chris Lea redis-server package, and the correct config file.

--- modules/instaredis/manifests/init.pp
+++ modules/instaredis/manifests/init.pp
@@ -1,11 +1,16 @@
 class instaredis {
+    apt::ppa { "ppa:chris-lea/redis-server": }
+
     package { "redis-server":
-        ensure => latest
+        ensure => latest,
+        require => Apt::Ppa["ppa:chris-lea/redis-server"],
     }
 
     file { "/etc/redis/redis.conf":
-        ensure => file,
-        content => template("redis/redis.conf.erb"),
+        owner       => root,
+        group       => root,
+        mode        => "0644",
+        content => template("instaredis/redis.conf.erb"),
         require => Package["redis-server"],
         notify => Service["redis-server"]
     }
