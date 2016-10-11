9934de299a450f55516af43274d1f7afc7035ec7
Tim Huegdon <tim@nefariousdesigns.co.uk>
2016-10-11 12:30:51 +0100

    Fix indentation on instaredis manifest.

--- modules/instaredis/manifests/init.pp
+++ modules/instaredis/manifests/init.pp
@@ -2,22 +2,22 @@ class instaredis {
     apt::ppa { "ppa:chris-lea/redis-server": }
 
     package { "redis-server":
-        ensure => latest,
-        require => Apt::Ppa["ppa:chris-lea/redis-server"],
+        ensure      => latest,
+        require     => Apt::Ppa["ppa:chris-lea/redis-server"],
     }
 
     file { "/etc/redis/redis.conf":
         owner       => root,
         group       => root,
         mode        => "0644",
-        content => template("instaredis/redis.conf.erb"),
-        require => Package["redis-server"],
-        notify => Service["redis-server"]
+        content     => template("instaredis/redis.conf.erb"),
+        require     => Package["redis-server"],
+        notify      => Service["redis-server"]
     }
 
     service { "redis-server":
-        ensure => running,
-        subscribe => File["/etc/redis/redis.conf"],
-        require => Package["redis-server"]
+        ensure      => running,
+        subscribe   => File["/etc/redis/redis.conf"],
+        require     => Package["redis-server"]
     }
 }
