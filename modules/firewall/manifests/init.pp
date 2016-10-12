8da45fe8ad79c9580522bb6f8fc64d353c6477ad
Tim Huegdon <tim@nefariousdesigns.co.uk>
2016-10-12 11:12:41 +0100

    Add two extra rules to the firewall.

--- modules/firewall/manifests/init.pp
+++ modules/firewall/manifests/init.pp
@@ -15,6 +15,14 @@ class firewall {
         port    => "443",
         proto   => "tcp",
     }
+    ufw::allow { "allow-monit-from-all":
+        port    => "2812",
+        proto   => "tcp",
+    }
+    ufw::allow { "allow-supervisorctl-from-all":
+        port    => "9009",
+        proto   => "tcp",
+    }
     ufw::allow { "allow-ntp-from-all":
         port    => "123",
         proto   => "udp",
